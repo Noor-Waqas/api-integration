a870453eb1dee9c5d22aab04b65bc72537609a9e
Mwaqas786 <onidea789@gmail.com>
2023-12-11 14:30:24 +0500

    add new post appi

--- src/compoment/sign-up/signUp.tsx
+++ src/compoment/sign-up/signUp.tsx
@@ -10,14 +10,15 @@ const signUp = () => {
         email:"",
         password:"",
     })
-    const onChangeHandle = (target: HTMLInputElement) =>   setuserdata( prevState =>   ({ ...prevState, [target.name]: target.value})) 
-    const SignUpHandler = async () => {
-        const response = await  axios.post("google.com/api/call", userdata)
+    const onChangeHandle = (target: HTMLInputElement) =>   setuserdata( prevState => ({ ...prevState, [target.name]: target.value})) 
+    const OnsubmitHandle = async () => {
+        const response = await  axios.post("https://645f31759d35038e2d1e99e2.mockapi.io/crud", userdata)
+        console.log("response",response)
+        
         if(response.status){
-            // redirect to login
+            console.log("Successfull Add DAta:")
         }else {
-            // error
-        }
+            console.log("Error")        }
     }
     return (
         <> 
@@ -25,7 +26,7 @@ const signUp = () => {
                 <h1 style={{display:"flex",justifyContent:"center"}}>SIGN UP</h1> 
             </div>
             <div style={{display:"flex",justifyContent:"center",marginTop:"1rem"}}>
-                <form >
+                <form onSubmit={OnsubmitHandle} >
                     <label >Username:</label><br/>
                     <input type="text" name="name" value={userdata.name} onChange={(e) => onChangeHandle(e.target)} /><br/>
                     <label >Phone:</label><br/>
@@ -38,7 +39,7 @@ const signUp = () => {
                     <input type="email" name="email" value={userdata.email} onChange={(e) => onChangeHandle(e.target)} /><br/>
                     <label >Password:</label><br/>
                     <input type="password" name="password" value={userdata.password} onChange={(e) => onChangeHandle(e.target)} /><br/><br/>
-                    <button type="submit" onClick={ SignUpHandler }>Sign Up</button>
+                    <button type="submit" >Sign Up</button>
                 </form>
             </div>
 
